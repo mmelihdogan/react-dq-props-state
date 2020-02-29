72966fe6994adad53aaa2dfe852023bf76f92279
mmelihdogan <doganmmelih@gmail.com>
2020-02-29 14:36:14 +0300

    weight done

--- src/BabyHog.js
+++ src/BabyHog.js
@@ -8,10 +8,20 @@ import GlowingBaby from './assets/glowing-eyes.png'
 // is there a way we could associate eye color string values with images? 
 // perhaps so we could do something along the lines of `eyeColorMapper['blue'] and get back the right image?`
 
+const eyeColorImgMap = {
+  'sun': SunBaby,
+  'blue': BlueBaby,
+  'glowing': GlowingBaby
+}
+
+
 export default class BabyHog extends Component {
 
   constructor(props) {
-    super(props)
+    super(props) 
+      this.state = {
+        weight: 30
+      }  
   }
 
   changeWeight = (e) => {
@@ -25,20 +35,20 @@ export default class BabyHog extends Component {
   render() {
     return (
       <li className="hogbabies">
-        <h1>Name</h1>
-        <h3>Weight:</h3>
-        <h3>Hobby:</h3>
-        <h4>Eye Color:</h4>
+        <h1>{this.props.name}</h1>
+        <h3>Weight: {this.state.weight}</h3>
+        <h3>Hobby: {this.props.hobby}</h3>
+    <h4>Eye Color:{this.props.eyeColor}</h4>
           
-        <Button name="+">
+        <Button name="+" onClick={this.changeWeight}>
           Increase Weight
         </Button>
-        <Button name="-">
+        <Button name="-" onClick={this.changeWeight}>
           Decrease Weight
         </Button>
 
         <div className="hb-wrap">
-          <img src={normalBaby} style={{height: '200px'}} alt="MasterBlasterJrJr" />
+          <img src={eyeColorImgMap[this.props.eyeColor]} style={{height: '200px'}} alt="MasterBlasterJrJr" />
         </div>
         
       </li>
--- src/MasterHog.js
+++ src/MasterHog.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import Master from './assets/master-hog.png'
 import BabyHog from './BabyHog'
-// import offspring from './db.js'
+import offspring from './db.js'
 
 export default class MasterHog extends Component {
 
@@ -21,6 +21,15 @@ export default class MasterHog extends Component {
 
 
   render() {
+    const renderBaby = (dbList) => {
+      return dbList.map(baby => {
+        return (
+          <BabyHog key={baby.id} name={baby.name} hobby={baby.hoby} weight={baby.weight} />
+        )
+      })
+    }
+
+
     return (
       <div>
 
@@ -30,19 +39,17 @@ export default class MasterHog extends Component {
         Sun<br></br>
         <input type="radio" name="eyeColor" value="glowing" onChange={this.changeEyeColor} />
         Glowing<br></br>
-      
+
         <h2>Name: Master Blaster</h2>
         <h3>Weight: 2.54 Tons</h3>
         <h3>Eye Color: {this.state.eyeColor}</h3>
-        
+
         <div id="masters-domicile">
           <img id="master-blaster" src={Master} alt="" />
         </div>
-        
+
         <ul className="hoglist">
-          <BabyHog />
-          <BabyHog />
-          <BabyHog />
+          { renderBaby(offspring) }
         </ul>
 
       </div>
